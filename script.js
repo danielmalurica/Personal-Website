29ef67b54a6867e8f50935ee5c001d3c5340cfec
danielmalurica <danielmalurica@gmail.com>
2023-09-05 11:38:04 +0300

    Js bug fix

--- script.js
+++ script.js
@@ -49,30 +49,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-// Firebase
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-analytics.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC6CXetvbuvprz6eRNAeV10OViyNn2xhYw",
-  authDomain: "mywebsite-c1a8a.firebaseapp.com",
-  projectId: "mywebsite-c1a8a",
-  storageBucket: "mywebsite-c1a8a.appspot.com",
-  messagingSenderId: "174452972257",
-  appId: "1:174452972257:web:3fa0e0f3a01b55cfa1f5a0",
-  measurementId: "G-R0CD4CHGTP",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-import { doc, setDoc } from "firebase/firestore";
-
-// Add a new document in collection "cities"
-await setDoc(doc(db, "cities", "LA"), {
-  name: "Los Angeles",
-  state: "CA",
-  country: "USA",
-});
